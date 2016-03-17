50b65e5eab90ed20d57d776cf325c0f6cb08a649
Kyle Kemp <kyle@seiyria.com>
2016-03-17 17:55:36 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs');
+
+const gulp = require('gulp');
+const iconfont = require('gulp-iconfont');
+const iconfontCss = require('gulp-iconfont-css');
+
+const ghPages = require('gulp-gh-pages');
+
+const FONT_NAME = 'game-icons';
+
+gulp.task('build:font', () => {
+    return gulp.src(['./icons/**/*.svg'])
+        .pipe(iconfontCss({
+            fontName: FONT_NAME,
+            formats: ['ttf', 'eot', 'woff']
+        }))
+        .pipe(iconfont({
+            fontName: FONT_NAME
+        }))
+        .on('glyphs', (glyphs) => {
+            fs.writeFile('./test/data/glyphs.json', JSON.stringify(glyphs));
+        })
+        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('test/css/'));
+});
+
+gulp.task('deploy', () => {
+    return gulp.src('./test/**/*')
+        .pipe(ghPages());
+});
---
+++ test/js/app.js
@@ -0,0 +1,64 @@
+
+const Data = {
+    glyphs: () => m.request({ method: 'GET', url: 'data/glyphs.json' })
+};
+
+const Filter = {
+    controller: function() {
+        this.searchTerm = m.prop('');
+    },
+    view: (ctrl) => {
+        return m('input.form-control', { placeholder: 'Filter icons...', oninput: m.withAttr('value', ctrl.searchTerm) });
+    }
+};
+
+const ForegroundColor = {
+    controller: function() {
+        this.fgColor = m.prop('');
+    },
+    view: (ctrl) => {
+        return m('input.form-control', { placeholder: 'Set FG Color', oninput: m.withAttr('value', ctrl.fgColor) });
+    }
+};
+
+const List = {
+    controller: function(options) {
+        return {
+            glyphs: Data.glyphs(),
+            visible: options.visible,
+            fgColor: options.fgColor
+        };
+    },
+    view: (ctrl) => {
+        return m('div.row', [
+            ctrl.glyphs().filter(ctrl.visible).map(glyph => m('div.col-md-1.col-sm-2.col-xs-3.icon-block.text-center', [
+                m(`i.icon.icon-${glyph.name}.icon-md`, { style: { color: ctrl.fgColor() }}),
+                m('br'),
+                glyph.name
+            ]))
+        ]);
+    }
+};
+
+const Page = {
+    controller: function() {
+        this.list = List.controller({
+            visible: (item) => item.name.indexOf(this.filter.searchTerm()) > -1,
+            fgColor: () => this.foregroundColor.fgColor()
+        });
+
+        this.foregroundColor = new ForegroundColor.controller();
+        this.filter = new Filter.controller();
+    },
+    view: (ctrl) => {
+        return [
+            m('div.row.text-center', [
+                m('div.col-sm-6', Filter.view(ctrl.filter)),
+                m('div.col-sm-6', ForegroundColor.view(ctrl.foregroundColor))
+            ]),
+            m('div.row', List.view(ctrl.list))
+        ];
+    }
+};
+
+m.mount(document.getElementById('page'), Page);
